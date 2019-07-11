4fef16a34610d4c5a46874f32c04b7f4621822ce
Anita Tenjarla <anita@cloudflare.com>
2019-07-11 13:32:24 -0700

    Fix flying off bug

--- FlappyBlock.js
+++ FlappyBlock.js
@@ -76,12 +76,20 @@ function component(width, height, color, x, y, type) {
         this.x += this.speedX;
         this.y += this.speedY + this.gravitySpeed;
         this.hitBottom();
+	this.hitTop();
     }
     this.hitBottom = function() {
         var rockbottom = myGameArea.canvas.height - this.height;
         if (this.y > rockbottom) {
             this.y = rockbottom;
             this.gravitySpeed = 0;
+        }
+    }
+    this.hitTop = function() {
+      var rockTop = 0;
+        if (this.y <= rockTop) {
+            this.y = rockTop;
+            this.gravitySpeed = (-1 * this.gravitySpeed);
         }
     }
     this.crashWith = function(otherobj) {
